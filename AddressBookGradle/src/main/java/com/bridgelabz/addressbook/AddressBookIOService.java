274bc011e1064fd34e04f7fb8877b2590503aa51
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-20 23:43:40 +0530

    REFACTOR: created class to provide IO service

---
+++ AddressBookGradle/src/main/java/com/bridgelabz/addressbook/AddressBookIOService.java
@@ -0,0 +1,54 @@
+package com.bridgelabz.addressbook;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AddressBookIOService {
+public static String ADDRESSBOOK_FILE_NAME = "AddressBook-file.txt";
+	
+	public void writeData(List<Contact> contacts) {
+		StringBuffer addressBuffer = new StringBuffer();
+		contacts.forEach(contact -> {
+			String employeeDataString = contact.toString().concat("\n");
+			addressBuffer.append(employeeDataString);
+		});
+		
+		try {
+			Files.write(Paths.get(ADDRESSBOOK_FILE_NAME),addressBuffer.toString().getBytes());
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void printData() {
+		try {
+			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath()).forEach(System.out::println);
+		} catch(IOException e) {
+			e.printStackTrace(); 
+		}
+	}
+	
+	public List<String> readDataFromFile() {
+			
+			List<String> addressBookList = new ArrayList<String>();
+			
+			try {
+				Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath())
+					.map(contact -> contact.trim())
+					.forEach(contact -> {
+						System.out.println(contact);
+						addressBookList.add(contact);
+				});
+				
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+			return addressBookList;
+		}
+
+}

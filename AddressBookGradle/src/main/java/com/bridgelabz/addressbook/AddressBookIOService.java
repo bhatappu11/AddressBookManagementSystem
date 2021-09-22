1860c900f28db35a4af3de3fa0daefa9503b83ea
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-22 22:11:12 +0530

    REFACTOR: used switch case to handle write and read using ioService

--- AddressBookGradle/src/main/java/com/bridgelabz/addressbook/AddressBookIOService.java
+++ AddressBookGradle/src/main/java/com/bridgelabz/addressbook/AddressBookIOService.java
@@ -13,7 +13,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.bridgelabz.addressbook.AddressBookList.IOService;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBean;
@@ -26,25 +28,116 @@ public static final String ADDRESSBOOK_CSV_FILE_NAME = "AddressBook-CSV.csv";
 private static final String ADDRESSBOOK_JSON_FILE_NAME = "./AddressBook-Json.json";
 
 	
-	public void writeData(List<Contact> contacts) {
-		StringBuffer addressBuffer = new StringBuffer();
-		contacts.forEach(contact -> {
-			String employeeDataString = contact.toString().concat("\n");
-			addressBuffer.append(employeeDataString);
-		});
+	public void writeData(List<Contact> contacts, IOService ioService) {
+		switch(ioService) {
+		case FILE_IO:
+			StringBuffer addressBuffer = new StringBuffer();
+			contacts.forEach(contact -> {
+				String employeeDataString = contact.toString().concat("\n");
+				addressBuffer.append(employeeDataString);
+			});
+			
+			try {
+				Files.write(Paths.get(ADDRESSBOOK_FILE_NAME),addressBuffer.toString().getBytes());
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		case CSV_IO:
+			try {
+	            Writer writer = Files.newBufferedWriter(Paths.get(ADDRESSBOOK_CSV_FILE_NAME));
+
+	            CSVWriter csvWriter = new CSVWriter(writer,
+	                   		CSVWriter.DEFAULT_SEPARATOR,
+	                   		CSVWriter.NO_QUOTE_CHARACTER,
+	                   		CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+	                   		CSVWriter.DEFAULT_LINE_END);
+	            List<String[]> data = new ArrayList();
+		        for(Contact c : contacts) {
+		        	String[] row = {c.getFirstName(),c.getLastName(),c.getCity(),c.getState(),c.getZip(),c.getPhoneNumber(),c.getEmail()};
+		        	data.add(row);
+		        }
+		        
+		        csvWriter.writeAll(data);
+		        csvWriter.close();
+
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		case JSON_IO:
+			Gson gson=new Gson();
+			String json=gson.toJson(contacts);
+			FileWriter writer;
+			try {
+				writer = new FileWriter(ADDRESSBOOK_JSON_FILE_NAME);
+				writer.write(json);	
+				writer.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		default:
+			System.err.println("Invalid choice");
+			
+		}
 		
-		try {
-			Files.write(Paths.get(ADDRESSBOOK_FILE_NAME),addressBuffer.toString().getBytes());
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
 	}
-	public void printData() {
-		try {
-			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath()).forEach(System.out::println);
-		} catch(IOException e) {
-			e.printStackTrace(); 
+	public void printData(IOService fileIo) {
+		switch(fileIo) {
+		case FILE_IO:
+			try {
+				Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath()).forEach(System.out::println);
+			} catch(IOException e) {
+				e.printStackTrace(); 
+			}
+			break;
+		case CSV_IO:
+			try {
+	            Reader reader = Files.newBufferedReader(Paths.get(ADDRESSBOOK_CSV_FILE_NAME));
+	            CSVReader csvReader = new CSVReader(reader);
+	        	List<String[]> records = csvReader.readAll();
+	        	for (String[] record : records) {
+	        		System.out.println("First Name: "+record[0]);
+	        		System.out.println("Last Name: "+record[1]);
+	        		System.out.println("City: "+record[2]);
+	        		System.out.println("State: "+record[3]);
+	        		System.out.println("Zip: "+record[4]);
+	        		System.out.println("Phone number: "+record[5]);
+	        		System.out.println("Email: "+record[6]);
+	        		System.out.println("============================");
+	        	}
+	        	
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		case JSON_IO:
+			Gson gson = new Gson();
+			List<Contact> contactList = new ArrayList<Contact>();
+			BufferedReader bufferObject;
+			try {
+				bufferObject = new BufferedReader(new FileReader(ADDRESSBOOK_JSON_FILE_NAME));
+				Contact[] addressBookData = gson.fromJson(bufferObject, Contact[].class);
+				contactList = Arrays.asList(addressBookData);
+				for(Contact contact : contactList) {
+					System.out.println("First name: "+contact.getFirstName());
+					System.out.println("Last name: "+contact.getLastName());
+					System.out.println("City: "+contact.getCity());
+					System.out.println("State: "+contact.getState());
+					System.out.println(("Zip: "+contact.getZip()));
+					System.out.println(("Phone number: "+contact.getPhoneNumber()));
+					System.out.println(("Email: "+contact.getEmail()));
+					System.out.println("=========================");
+				}
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			break;
+		default:
+			System.err.println("Invalid choice");
 		}
+		
 	}
 	
 	public List<String> readDataFromFile() {
@@ -65,84 +158,6 @@ private static final String ADDRESSBOOK_JSON_FILE_NAME = "./AddressBook-Json.jso
 			}
 			return addressBookList;
 		}
-	public void writeDataToCsv(List<Contact> contacts){
-		 try {
-		            Writer writer = Files.newBufferedWriter(Paths.get(ADDRESSBOOK_CSV_FILE_NAME));
-
-		            CSVWriter csvWriter = new CSVWriter(writer,
-		                   		CSVWriter.DEFAULT_SEPARATOR,
-		                   		CSVWriter.NO_QUOTE_CHARACTER,
-		                   		CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-		                   		CSVWriter.DEFAULT_LINE_END);
-		            List<String[]> data = new ArrayList();
-			        for(Contact c : contacts) {
-			        	String[] row = {c.getFirstName(),c.getLastName(),c.getCity(),c.getState(),c.getZip(),c.getPhoneNumber(),c.getEmail()};
-			        	data.add(row);
-			        }
-			        
-			        csvWriter.writeAll(data);
-			        csvWriter.close();
-
-		 }catch(IOException e) {
-			 e.printStackTrace();
-		 }
 		
-	}
-	public void readFromCsv() {
-		try {
-	            Reader reader = Files.newBufferedReader(Paths.get(ADDRESSBOOK_CSV_FILE_NAME));
-	            CSVReader csvReader = new CSVReader(reader);
-	        	List<String[]> records = csvReader.readAll();
-	        	for (String[] record : records) {
-	        		System.out.println("First Name: "+record[0]);
-	        		System.out.println("Last Name: "+record[1]);
-	        		System.out.println("City: "+record[2]);
-	        		System.out.println("State: "+record[3]);
-	        		System.out.println("Zip: "+record[4]);
-	        		System.out.println("Phone number: "+record[5]);
-	        		System.out.println("Email: "+record[6]);
-	        		System.out.println("============================");
-	        	}
-	        	
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public void readDataFromJson() {
-	Gson gson = new Gson();
-	List<Contact> contactList = new ArrayList<Contact>();
-	BufferedReader bufferObject;
-	try {
-		bufferObject = new BufferedReader(new FileReader(ADDRESSBOOK_JSON_FILE_NAME));
-		Contact[] addressBookData = gson.fromJson(bufferObject, Contact[].class);
-		contactList = Arrays.asList(addressBookData);
-		for(Contact contact : contactList) {
-			System.out.println("First name: "+contact.getFirstName());
-			System.out.println("Last name: "+contact.getLastName());
-			System.out.println("City: "+contact.getCity());
-			System.out.println("State: "+contact.getState());
-			System.out.println(("Zip: "+contact.getZip()));
-			System.out.println(("Phone number: "+contact.getPhoneNumber()));
-			System.out.println(("Email: "+contact.getEmail()));
-			System.out.println("=========================");
-		}
-	}catch(IOException e) {
-		e.printStackTrace();
-	}
-	}
-	
-	public void writeDataToJson(List<Contact> contacts) {
-		Gson gson=new Gson();
-		String json=gson.toJson(contacts);
-		FileWriter writer;
-		try {
-			writer = new FileWriter(ADDRESSBOOK_JSON_FILE_NAME);
-			writer.write(json);	
-			writer.close();
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
 
 }

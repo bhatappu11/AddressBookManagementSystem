9f6c616e978142a12412d4f8402798c8fd026d22
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-29 22:52:22 +0530

    REFACTORED: override equals method

--- AddressBookGradle/src/main/java/com/bridgelabz/addressbook/Contact.java
+++ AddressBookGradle/src/main/java/com/bridgelabz/addressbook/Contact.java
@@ -1,19 +1,20 @@
 package com.bridgelabz.addressbook;
 
 public class Contact implements Comparable<Contact>{
-	private String firstName;
-	private String lastName;
-	private String city;
-	private String state;
-	private String zip;
-	private String phoneNumber;
-	private String email;
-	private String id;
+	public String firstName;
+	public String lastName;
+	public String city;
+	public String state;
+	public String zip;
+	public String phoneNumber;
+	public String email;
+	public String id;
+	public AddressBookList bookID;
 	public String getId() {
 		return id;
 	}
 
-	public String getBookID() {
+	public AddressBookList getBookID() {
 		return bookID;
 	}
 
@@ -21,11 +22,11 @@ public class Contact implements Comparable<Contact>{
 		this.id = id;
 	}
 
-	public void setBookID(String bookID) {
+	public void setBookID(AddressBookList bookID) {
 		this.bookID = bookID;
 	}
 
-	private String bookID;
+	
 	
 	public Contact(String firstName, String lastName, String city, String state, String zip, String phoneNumber,
 			String email) {
@@ -38,13 +39,13 @@ public class Contact implements Comparable<Contact>{
 		this.email = email;
 	}
 
-	public Contact(String id, String firstName2, String lastName2, String phoneNumber2, String email2, String bookID) {
+	public Contact(String id, String firstName2, String lastName2, String phoneNumber2, String email2, AddressBookList addressBookList) {
 		this.id = id;
 		this.firstName = firstName2;
 		this.lastName = lastName2;
 		this.phoneNumber = phoneNumber2;
 		this.email = email2;
-		this.bookID = bookID;
+		this.bookID = addressBookList;
 	}
 
 	public String getFirstName() {
@@ -107,71 +108,19 @@ public class Contact implements Comparable<Contact>{
 		return this.firstName.compareTo(c.getFirstName());
 	}
 
+	
 	@Override
 	public String toString() {
-		return "Contact [FirstName=" + firstName + ", LastName=" + lastName + ", City=" + city + ", State=" + state
-				+ ", Zip=" + zip + ", PhoneNumber=" + phoneNumber + ", Email=" + email + "]";
+		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state=" + state
+				+ ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + ", id=" + id + ", bookID="
+				+ bookID + "]";
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((city == null) ? 0 : city.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
-		result = prime * result + ((state == null) ? 0 : state.hashCode());
-		result = prime * result + ((zip == null) ? 0 : zip.hashCode());
-		return result;
+    public boolean equals(Object object) {
+    	if(object == this)  return true;
+    	if(!(object instanceof Contact)) return false;
+    	Contact person1 = (Contact) object;
+    	return (this.firstName.equals(person1.firstName)  && this.lastName.equals(person1.lastName) && this.phoneNumber.equals(person1.phoneNumber)); 
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Contact other = (Contact) obj;
-		if (city == null) {
-			if (other.city != null)
-				return false;
-		} else if (!city.equals(other.city))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		if (phoneNumber == null) {
-			if (other.phoneNumber != null)
-				return false;
-		} else if (!phoneNumber.equals(other.phoneNumber))
-			return false;
-		if (state == null) {
-			if (other.state != null)
-				return false;
-		} else if (!state.equals(other.state))
-			return false;
-		if (zip == null) {
-			if (other.zip != null)
-				return false;
-		} else if (!zip.equals(other.zip))
-			return false;
-		return true;
-	}
-	
 }

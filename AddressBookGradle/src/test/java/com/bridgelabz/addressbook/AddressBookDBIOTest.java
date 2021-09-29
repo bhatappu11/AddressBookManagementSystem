d064964d85f64473081690c450d87da36096fa1e
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-29 22:55:48 +0530

    REFACTORED: test class for database methods

--- AddressBookGradle/src/test/java/com/bridgelabz/addressbook/AddressBookDBIOTest.java
+++ AddressBookGradle/src/test/java/com/bridgelabz/addressbook/AddressBookDBIOTest.java
@@ -1,11 +1,5 @@
 package com.bridgelabz.addressbook;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
@@ -13,9 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.bridgelabz.addressbook.AddressBookService.IOService;
-import com.google.gson.Gson;
 
-public class AddressBookTest {
+public class AddressBookDBIOTest {
 	public Contact contact1;
 	public Contact contact2;
 	@Before
@@ -26,111 +19,28 @@ public class AddressBookTest {
 	}
 	
 	@Test
-	public void givenContactDetails_ShouldAddToAddressBook() {
-		AddressBook book1 = new AddressBook();
-		book1.addContact(contact1);
-		book1.addContact(contact2);
-		Assert.assertEquals(2, book1.contacts.size());
-		
-	}
-	
-	@Test
-	public void givenContacts_ShouldAddToTextFile() {
-		AddressBook book1 = new AddressBook();
-		book1.addContact(contact1);
-		book1.addContact(contact2);
-		book1.writeDataToFile(IOService.FILE_IO);
-		long size=0;
-		try {
-			size = Files.lines(Paths.get(AddressBookIOService.ADDRESSBOOK_FILE_NAME)).count();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		Assert.assertEquals(2,size);		
-	}
-	@Test
-	public void givenContacts_ShouldAddToCSVFile() {
-		AddressBook book1 = new AddressBook();
-		book1.addContact(contact1);
-		book1.addContact(contact2);
-		book1.writeDataToFile(IOService.CSV_IO);
-		long size=0;
-		try {
-			size = Files.lines(Paths.get(AddressBookIOService.ADDRESSBOOK_CSV_FILE_NAME)).count();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		Assert.assertEquals(2,size);		
-	}
-	@Test
-	public void givenContacts_ShouldAddToJSONFile() {
-		AddressBook book1 = new AddressBook();
-		book1.addContact(contact1);
-		book1.addContact(contact2);
-		book1.writeDataToFile(IOService.JSON_IO);
-		long size=0;
-		try {
-			Gson gson = new Gson();
-			BufferedReader br = new BufferedReader(new FileReader(AddressBookIOService.ADDRESSBOOK_JSON_FILE_NAME));
-			Contact[] usrObj = gson.fromJson(br, Contact[].class);
-			List<Contact> csvUSerList = Arrays.asList(usrObj);
-			size = csvUSerList.size();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		Assert.assertEquals(2,size);		
-	}
-	@Test
-	public void whenReadFromCSVMethodCalled_ShouldPrintFile() {
-		AddressBook book1 = new AddressBook();
-		book1.addContact(contact1);
-		book1.addContact(contact2);
-		book1.writeDataToFile(IOService.CSV_IO);	
-		book1.readDataFromFile(IOService.CSV_IO);
-		Assert.assertTrue(true);
-	}
-	@Test
-	public void whenReadFromTextMethodCalled_ShouldPrintFile() {
-		AddressBook book1 = new AddressBook();
-		book1.addContact(contact1);
-		book1.addContact(contact2);
-		book1.writeDataToFile(IOService.FILE_IO);	
-		book1.readDataFromFile(IOService.FILE_IO);
-		Assert.assertTrue(true);
-	}
-	@Test
-	public void whenReadFromJsonMethodCalled_ShouldPrintFile() {
-		AddressBook book1 = new AddressBook();
-		book1.addContact(contact1);
-		book1.addContact(contact2);
-		book1.writeDataToFile(IOService.JSON_IO);	
-		book1.readDataFromFile(IOService.JSON_IO);
-		Assert.assertTrue(true);
-	}
-	@Test
 	public void givenContactInDB_WhenRetrieved_ShouldMatchContactCount(){
 		AddressBookService addressBookService = new AddressBookService();
 		List<Contact> contactList = addressBookService.readContact(IOService.DB_IO);
-		Assert.assertEquals(6, contactList.size());
+		System.out.println(contactList);
+		Assert.assertEquals(2, contactList.size());
 	}
 	@Test
-	public void givenAConact_WhenInserted_ShouldGetUpdatedSize(){
-		Contact contact = new Contact("C8", "Jasmine", "Ruby", "7895679067", "jas@gmail.com", "b2");
+	public void givenAContact_WhenAdded_ShouldSyncWithDB(){
 		AddressBookService addressBookService = new AddressBookService();
-		long initialSize  = addressBookService.readContact(IOService.DB_IO).size();
-		addressBookService.writeContact(contact,IOService.DB_IO);
-		long updatedSize = addressBookService.readContact(IOService.DB_IO).size();
-		Assert.assertEquals(initialSize+1, updatedSize);
-	}
+		addressBookService.readContact(IOService.DB_IO);
+		addressBookService.addContact("c20","Pooja","lal","8845234567","rit@gmail.com","b3","a9","Banglore","Karnataka","657345","t3");
+		//List<Contact> contactList = addressBookService.readContact(IOService.DB_IO);
+		boolean result=addressBookService.checkContactInSyncWithDB("c20");
+		Assert.assertTrue(result);
+	}/*
 	@Test
 	public void givenACity_WhenQueried_ShouldGetNumberOfContacts()
 	{
 		AddressBookService addressBookService = new AddressBookService();
 		int count  = addressBookService.countByCity("Blore", IOService.DB_IO);
 		Assert.assertEquals(2, count);
-	}
+	}*/
+	
 	
 }
